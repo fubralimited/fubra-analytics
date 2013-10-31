22fd1aa5ca2f93638b1c2e19bec58f761425547f
Ray Viljoen <rayviljoen@me.com>
2013-10-31 17:16:20 +0000

    Adds color change utility

--- lib/FA/Util.php
+++ lib/FA/Util.php
@@ -32,5 +32,60 @@ class Util {
         
     }
 
+    /**
+     * Get hex color value for percentage change
+     * @param  int  $val   Percentage change that occured
+     * @param  int  $point Zero point to base calculation on
+     * @param  bool $point Rever green & red
+     * @return string   Hex string
+     */
+    public static function percentage_color($val, $point = 0, $reverse = false) {
+
+        // Get absolute value
+        $abs = abs($val - $point);
+
+        // Base value
+        $base = 100 - $point;
+
+        // Set no more than base value
+        if ($abs >= $base) $abs = $base;
+
+        // Calculate percentage of 255
+        $var_c = round( ($abs/$base) * 255 );
+
+        // Get remaing value of 255
+        $remain = round( (255 - $var_c) );
+
+        // Check which side the value needs to be
+        if ($reverse) {
+
+            $rgb = ($val > $point) ? array( 255, $remain, $remain ) : array( $remain , 255, $remain );
+        
+        } else {
+
+            $rgb = ($val < $point) ? array( 255, $remain, $remain ) : array( $remain , 255, $remain );
+        }
+
+        return self::rgb_to_hx($rgb);
+    }
+
+
+    /**
+     * Converts rgb to a hex string
+     * @param  [type] $r [description]
+     * @param  [type] $g [description]
+     * @param  [type] $b [description]
+     * @return [type]    [description]
+     */
+    public static function rgb_to_hx($rgb) {
+
+        $hex = "#";
+        $hex .= str_pad(dechex($rgb[0]), 2, "0", STR_PAD_LEFT);
+        $hex .= str_pad(dechex($rgb[1]), 2, "0", STR_PAD_LEFT);
+        $hex .= str_pad(dechex($rgb[2]), 2, "0", STR_PAD_LEFT);
+
+        return $hex;
+    }
+
 
 }
