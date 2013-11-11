e3b0979c17ff848908f2a3a1ba67100fcc946ac5
Ray Viljoen <rayviljoen@me.com>
2013-11-11 13:44:10 +0000

    Adds composer check and install prompt

--- http/index.php
+++ http/index.php
@@ -1,7 +1,10 @@
 <?php
 
-// Autoload Composer modules
-require dirname(__DIR__) . '/composer/vendor/autoload.php';
+
+// Autoload Composer modules else prompt for install
+$composer_auto = dirname(__DIR__) . '/composer/vendor/autoload.php';
+if (file_exists($composer_auto)) { require $composer_auto; }
+else { echo 'Could not load composer modules.<br/>Run install script or see <a target="_blank" href="https://github.com/fubralimited/fubra-analytics">https://github.com/fubralimited/fubra-analytics</a> for more info.'; die(); }
 
 // Get configuration.ini file
 $config = new \FA\Config();
