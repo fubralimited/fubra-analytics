79d994ebfb25159b042b1514cb39b20598fcb800
Ray Viljoen <rayviljoen@me.com>
2013-11-13 12:03:14 +0000

    Adds htaccess to ignore file and uses installer script to create htaccess file.

--- install/install.sh
+++ install/install.sh
@@ -19,6 +19,8 @@ This script will install ${APP}:
 # Get install directory
 INSTALL_DIR=$(dirname $0)
 
+# Get install directory
+HTACCESS_DIR=$INSTALL_DIR/../http/.htaccess
 
 # Prompt to confirm install
 read -p "Do you want to continue? (y/n)" -n 1
@@ -28,6 +30,22 @@ read -p "Do you want to continue? (y/n)" -n 1
     if [[ $REPLY =~ ^[Yy]$ ]]; then
             
         # Continue with install
+
+        # Check for .htaccess file
+        if [ -f $HTACCESS_DIR ]; then
+
+            echo -e "* .htaccess file found \n"
+
+        # Create .htaccess file
+        else
+            echo -e "* Creating .htaccess file\n"
+            echo "RewriteEngine on" >> $HTACCESS_DIR
+            echo "RewriteCond %{REQUEST_FILENAME} !-f" >> $HTACCESS_DIR
+            echo "RewriteRule ^(.*)$ index.php [QSA,L]" >> $HTACCESS_DIR
+            echo -e "\n# Enable short tags" >> $HTACCESS_DIR
+            echo "php_value short_open_tag 1" >> $HTACCESS_DIR
+
+        fi
         
         # Check for config file and continue if found
         if [ -f $INSTALL_DIR/../config.ini ]; then
